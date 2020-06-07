4a08d388534bf5b30cc0f8dd5779bb1797beb832
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-07 10:11:39 -0400

    functionalize

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # pypistats-plots
 
-Plots using [pypistats API](https://github.com/hugovk/pypistats).
+![ci](https://github.com/scivision/pypistats-plots/workflows/ci/badge.svg)
+
+Plots using
+[pypistats API](https://github.com/hugovk/pypistats).
 As it is currently, the PyPiStats API seems to be more "printing tables" oriented rather than "comparing plots" oriented.
 Thus we created this script to plot data instead.
 Our first interest in creating this addendum project was to gauge relative use of minor Python versions to decide when to cutoff EOL Python versions e.g. Python 3.5.
 
 ![Meson weekly](./data/Meson_weekly_pypi.png)
+
+![Numpy](./data/numpy_2019-12.png)
+
+![pymap3d](./data/pymap3d_2019-12.png)
+
+![requests](./data/requests_2019-12.png)
 
 ## Usage
 
--- plot_pypistats.py
+++ plot_pypistats.py
@@ -3,19 +3,23 @@
 Plots Python package stats by Python version.
 """
 import pypistats as ps
+
+import pandas
+import numpy as np
 from matplotlib.pyplot import figure, show
+
 import argparse
 import json
-import pandas
+import typing as T
 
 
-def download_stats(projname: str) -> pandas.DataFrame:
+def download_stats(projname: str, versions: T.Sequence[str]) -> pandas.DataFrame:
     endpoint = f"packages/{projname}/python_minor"
     params = ps._paramify("mirrors", False)
     dat_json = ps.pypi_stats_api(endpoint, params=params, total=None, format="json")
     dat = json.loads(dat_json)
 
-    df = pandas.DataFrame(columns=["3.4", "3.5", "3.6", "3.7", "3.8", "3.9"])
+    df = pandas.DataFrame(columns=versions)
     for d in dat["data"]:
         if d["category"] not in df.columns:
             continue
@@ -23,35 +27,58 @@ def download_stats(projname: str) -> pandas.DataFrame:
 
     df.index = pandas.to_datetime(df.index)
 
+    df.attrs.update({"project_name": projname})
+
     return df
+
+
+def normalize(dat: pandas.DataFrame) -> pandas.DataFrame:
+
+    weekly_all = dat.resample(f"{dat.attrs['window']}W").sum()
+    weekly = weekly_all[versions]
+    total = weekly.sum(axis=1)
+    normed = (weekly.T / total).T
+    normed.attrs = dat.attrs
+    return normed
+
+
+def plot_trends(normed: pandas.DataFrame, versions: T.Sequence[str]):
+
+    fg = figure()
+    axs = fg.subplots(2, 1, sharex=True)
+    normed.plot(ax=axs[0])
+    axs[0].set_title(
+        f"{normed.attrs['project_name']}: PyPi downloads by Python version (pypistats.org): {normed.attrs['window']} week average"
+    )
+    axs[0].set_ylabel("normalized downloads")
+    axs[0].legend(loc="center left")
+
+    lost = normed[versions[0]]
+    current = normed[versions[1:]].sum(axis=1)
+
+    ax = axs[1]
+    current.plot(ax=ax, label=f"{versions[1]}..{versions[-1]}")
+    lost.plot(ax=ax, label=f"{versions[0]}")
+    ax.set_ylabel("normalized downloads")
+    ax.legend(loc="center left")
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("projname", help="PyPi project name")
-    p.add_argument("-w", "--window", help="time window to average over (weeks)", type=int, default=1)
+    p.add_argument(
+        "-w", "--window", help="time window to average over (weeks)", type=int, default=1
+    )
+    p.add_argument("-old", help="old Python version to consider dropping", type=float, default=3.5)
+    p.add_argument("-new", help="newest Python version to consider", type=float, default=3.9)
     P = p.parse_args()
 
-    dat = download_stats(P.projname)
-    weekly_all = dat.resample(f"{P.window}W").sum()
-    weekly = weekly_all[["3.5", "3.6", "3.7", "3.8", "3.9"]]
-    total = weekly.sum(axis=1)
-    normed = (weekly.T / total).T
+    versions = np.around(np.arange(P.old, P.new, 0.1), 1).astype(str)
 
-    fg = figure()
-    axs = fg.subplots(2, 1, sharex=True)
-    normed.plot(ax=axs[0])
-    axs[0].set_title(f"{P.projname}: PyPi downloads by Python version (pypistats.org): {P.window} week average")
-    axs[0].set_ylabel("normalized downloads")
-    axs[0].legend(loc="center left")
+    dat = download_stats(P.projname, versions)
+    dat.attrs["window"] = P.window
+    normed = normalize(dat)
 
-    lost = normed["3.5"]
-    current = normed[["3.6", "3.7", "3.8", "3.9"]].sum(axis=1)
-
-    ax = axs[1]
-    current.plot(ax=ax, label="3.6..3.9")
-    lost.plot(ax=ax, label="3.5")
-    ax.set_ylabel("normalized downloads")
-    ax.legend(loc="center left")
+    plot_trends(normed, versions)
 
     show()
