6e6d8c2240bea66f9c6285315c9bd4406a5fe256
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-13 17:09:44 -0400

    handle NaNs, Python 3.10

--- plot_pypistats.py
+++ plot_pypistats.py
@@ -2,18 +2,18 @@
 """
 Plots Python package stats by Python version.
 """
+
+from __future__ import annotations
 import pypistats as ps
 
 import pandas
-import numpy as np
 from matplotlib.pyplot import figure, show
 
 import argparse
 import json
-import typing as T
 
 
-def download_stats(projname: str, versions: T.Sequence[str]) -> pandas.DataFrame:
+def download_stats(projname: str, versions: list[str]) -> pandas.DataFrame:
     endpoint = f"packages/{projname}/python_minor"
     params = ps._paramify("mirrors", False)
     dat_json = ps.pypi_stats_api(endpoint, params=params, total=None, format="json")
@@ -23,18 +23,19 @@ def download_stats(projname: str, versions: T.Sequence[str]) -> pandas.DataFrame
     for d in dat["data"]:
         if d["category"] not in df.columns:
             continue
+
         df.loc[d["date"], d["category"]] = d["downloads"]
 
     df.index = pandas.to_datetime(df.index)
 
     df.attrs.update({"project_name": projname})
+    df = df.sort_index().fillna(0)
 
     return df
 
 
-def normalize(dat: pandas.DataFrame) -> pandas.DataFrame:
-
-    weekly_all = dat.resample(f"{dat.attrs['window']}W").sum()
+def normalize(dat: pandas.DataFrame, versions: list[str]) -> pandas.DataFrame:
+    weekly_all = dat.resample(f"{dat.attrs['window']}W", axis=0).sum()
     weekly = weekly_all[versions]
     total = weekly.sum(axis=1)
     normed = (weekly.T / total).T
@@ -42,7 +43,7 @@ def normalize(dat: pandas.DataFrame) -> pandas.DataFrame:
     return normed
 
 
-def plot_trends(normed: pandas.DataFrame, versions: T.Sequence[str]):
+def plot_trends(normed: pandas.DataFrame, versions: list[str]):
 
     fg = figure()
     axs = fg.subplots(2, 1, sharex=True)
@@ -69,23 +70,19 @@ if __name__ == "__main__":
     p.add_argument(
         "-w", "--window", help="time window to average over (weeks)", type=int, default=1
     )
-    p.add_argument("-old", help="old Python version to consider dropping", type=float, default=3.5)
-    p.add_argument("-new", help="newest Python version to consider", type=float, default=3.9)
+    p.add_argument(
+        "-v",
+        "--versions",
+        help="Python minor versions to consider",
+        nargs="+",
+        default=["3.5", "3.6", "3.7", "3.8", "3.9", "3.10"],
+    )
     P = p.parse_args()
 
-    if P.old < 3.4:
-        v = np.around(np.arange(P.old, 2.7 + 0.1, 0.1), 1)
-        if P.new > 3.4:
-            v = np.append(v, np.around(np.arange(3.5, P.new + 0.1, 0.1), 1))
-    else:
-        v = np.around(np.arange(P.old, P.new + 0.1, 0.1), 1)
-
-    versions = v.astype(str)
-
-    dat = download_stats(P.projname, versions)
+    dat = download_stats(P.projname, P.versions)
     dat.attrs["window"] = P.window
-    normed = normalize(dat)
+    normed = normalize(dat, P.versions)
 
-    plot_trends(normed, versions)
+    plot_trends(normed, P.versions)
 
     show()
